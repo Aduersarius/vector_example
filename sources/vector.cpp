2994286732a12e4a0e8f7885cd4aeb2b9f6136e9
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-06-18 20:34:21 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -67,16 +67,20 @@ void vector_t::push_back(int value)
 	if (size == capacity){
 		size++;
 		capacity *= 2;
-		int* tmp = new int[capacity];
+		if (int* tmp = new int[capacity])
+			throw "недостаточно памяти";
+		else{
 		for (int i = 0; i < size; i++)
 	        tmp[i] = els[i];
 		delete [] els;
-		els = new int[capacity];
+		if (els = new int[capacity])
+			throw "недостаточно памяти"
+			else {
 		for (int i = 0; i < size-1; i++)
 	        els[i] = tmp[i];
 		delete [] tmp;
 		els[size-1] = value;
-	}
+	} } }
 	else {  els[size] = value;
 	        size++;
 	     }
